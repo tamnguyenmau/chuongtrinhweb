ed2f4a673437cd3687d4baec63d9fac689c4b55d
tamnguyenmau <nguyenmautam123@gmail.com>
2022-08-15 21:24:34 +0700

    chuongtrinhweb

---
+++ news/js/main.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu')
+document.onscroll = function() {
+    const scrollLength = window.scrollY
+    if (scrollLength > 200) {
+        menu.classList.add("sticky")
+    }
+    if (scrollLength === 0){
+        menu.classList.remove("sticky")
+    }
+}
